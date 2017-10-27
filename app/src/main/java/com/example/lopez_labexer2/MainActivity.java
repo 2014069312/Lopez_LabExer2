8131e9369833e0f5bff5744d92957f99a8bce22c
Jessica Lopez <2014069312@ust-ics.mygbiz.com>
2017-10-27 20:50:47 +0800

    Updated functionality

--- app/src/main/java/com/example/lopez_labexer2/MainActivity.java
+++ app/src/main/java/com/example/lopez_labexer2/MainActivity.java
@@ -1,13 +1,76 @@
 package com.example.lopez_labexer2;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText etUsername, etPassword;
+    Button btnSharedPreferences, btnInternalStorage, btnNext;
+    SharedPreferences preferences;
+    FileOutputStream fos;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        etUsername = (EditText) findViewById(R.id.et_username);
+        etPassword = (EditText) findViewById(R.id.et_password);
+        btnSharedPreferences = (Button) findViewById(R.id.btn_sharedPreferences);
+        btnInternalStorage = (Button) findViewById(R.id.btn_internalStorage);
+        btnNext = (Button) findViewById(R.id.btn_next);
+        preferences = getSharedPreferences("pref", Context.MODE_WORLD_READABLE);
+    }
+
+    public void saveSharedPreference (View view) {
+        SharedPreferences.Editor editor = preferences.edit();
+        editor.putString("user", etUsername.getText().toString());
+        editor.putString("pwd", etPassword.getText().toString());
+        editor.commit();
+
+        Toast.makeText(this, "Saved in Shared Preferences!", Toast.LENGTH_SHORT).show();
+    }
+
+    public void saveInternalStorage (View view) {
+        String username = etUsername.getText().toString();
+        String password = etPassword.getText().toString();
+        String message1 = "Username is ";
+        String message2 = " and password is ";
+        try {
+            fos = openFileOutput("output.txt", Context.MODE_PRIVATE);
+            fos.write(message1.getBytes());
+            fos.write(username.getBytes());
+            fos.write(message2.getBytes());
+            fos.write(password.getBytes());
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                fos.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        Toast.makeText(this, "Saved in Internal Storage!", Toast.LENGTH_SHORT).show();
+    }
+
+    public void goToNext (View view) {
+        Intent intent = new Intent(this, SecondActivity.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/example/lopez_labexer2/SecondActivity.java
+++ app/src/main/java/com/example/lopez_labexer2/SecondActivity.java
@@ -1,13 +1,67 @@
 package com.example.lopez_labexer2;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
 public class SecondActivity extends AppCompatActivity {
+
+    Button btnLoadSharedPreferences, btnLoadInternalStorage, btnClear, btnBack;
+    TextView tvDisplay;
+    FileInputStream fis;
+    SharedPreferences preferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+        btnLoadSharedPreferences = (Button) findViewById(R.id.btn_loadSharedPreferences);
+        btnLoadInternalStorage = (Button) findViewById(R.id.btn_loadInternalStorage);
+        btnClear = (Button) findViewById(R.id.btn_clear);
+        btnBack = (Button) findViewById(R.id.btn_back);
+        tvDisplay = (TextView) findViewById(R.id.tv_display);
+        preferences = getSharedPreferences("pref",Context.MODE_WORLD_READABLE);
+    }
+
+    public void loadSharedPreferences (View view) {
+        String username = preferences.getString("user", "");
+        String password = preferences.getString("pwd", "");
+
+        tvDisplay.setText("The user is " + username + " and the password is " + password );
+    }
+
+    public void loadInternalStorage (View view) {
+        StringBuffer buffer = new StringBuffer();
+        int read = 0;
+        try {
+            fis = openFileInput("output.txt");
+            while ((read = fis.read()) != -1) {
+                buffer.append((char) read);
+            } fis.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        tvDisplay.setText(buffer.toString());
+    }
+
+    public void clearText (View view) {
+        tvDisplay.setText("");
+    }
+
+    public void goBack (View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 }
